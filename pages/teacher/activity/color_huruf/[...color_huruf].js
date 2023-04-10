6684b8fc6976aafdb9e1a9020db69f248fd67152
asus <someshanjana26@gmail.com>
2023-04-10 17:48:45 +0530

    generic routing for all the activities, dynamically created color hurf/alfazz activity, route changes

---
+++ pages/teacher/activity/color_huruf/[...color_huruf].js
@@ -0,0 +1,59 @@
+import ActivityDetail from "@/components/Modules/models/ActivityDetail";
+// import LetterDetails from "@/components/layout/LetterDetails";
+import { useRouter } from "next/router";
+import React, { useContext, useEffect } from "react";
+import colorBgImg from "@/components/src/img/colorBgImg.png";
+import AlphabetSlider from "@/components/Modules/models/AlphabetSlider";
+import logo from "@/components/src/img/AMLogo.png";
+import AuthContext from "@/components/Context/store/auth-context";
+import CanvasForWords from "@/components/Modules/Canvas/CanvasForWords";
+
+const index = () => {
+  const router = useRouter();
+
+  let subModule;
+  let module;
+  let activityIndex;
+
+  if (router.query.color_huruf) {
+    module = router.query.color_huruf[0];
+    subModule = router.query.color_huruf[1];
+    activityIndex = router.query.color_huruf[2];
+  }
+
+  console.log(subModule);
+  console.log(module);
+
+  const authCtx = useContext(AuthContext);
+
+  const loggedIn = authCtx.isLoggedIn;
+
+  const typeStudent = authCtx.userType === "student" ? true : false;
+
+  return (
+    <>
+      <div
+        className=""
+        style={{
+          backgroundImage: `url(${colorBgImg.src})`,
+          backgroundAttachment: "fixed",
+          backgroundSize: "100%",
+          backgroundPosition: "center top",
+          widows: "100vw",
+          minHeight: "100vh",
+        }}
+      >
+        {subModule && module && activityIndex && (
+          <CanvasForWords
+            user="teacher"
+            subModule={subModule}
+            module={module}
+            activityIndex={activityIndex}
+          />
+        )}
+      </div>
+    </>
+  );
+};
+
+export default index;
---
+++ pages/teacher/activity/color_huruf/index.js
@@ -0,0 +1,12 @@
+import CanvasForWords from "@/components/Modules/Canvas/CanvasForWords";
+import React from "react";
+
+const index = () => {
+  return (
+    <div>
+      <CanvasForWords />
+    </div>
+  );
+};
+
+export default index;
